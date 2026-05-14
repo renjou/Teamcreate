c4c4e8c50cfd93348488a59694807f49640ed331
yamagatamasaki <fko2647067@stu.o-hara.ac.jp>
2026-05-14 18:44:01 +0900

    up

--- NARAKURASSHU/Assets/yamagata/AutoMove1.cs
+++ NARAKURASSHU/Assets/yamagata/AutoMove1.cs
@@ -1,12 +1,26 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class AutoMove : MonoBehaviour
  {
-    public float speed = 3.0f;
-
+    public float speed = 1.0f;
+    private int direction = 1; 
 
     void Update()
     {
-        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        transform.position += new Vector3(direction*speed * Time.deltaTime, 0, 0);
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("wall"))
+        
+            direction = -1;
+
+        Vector3 scale = transform.localScale;
+        scale.x = -1;
+        transform.localScale = scale;
+    
+  }
 }
