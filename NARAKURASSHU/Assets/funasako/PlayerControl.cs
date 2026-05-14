0f9273817b609e18bf412014c7789cbde269d8e0
FunasakoYuga <fko2647071@stu.o-hara.ac.jp>
2026-05-14 18:41:48 +0900

    up

--- NARAKURASSHU/Assets/funasako/PlayerControl.cs
+++ NARAKURASSHU/Assets/funasako/PlayerControl.cs
@@ -6,6 +6,8 @@ public class PlayerControl : MonoBehaviour
     Rigidbody2D PlayerRigid;
     float jumpforce = 300;
     float speed = 5.0f; // 移動速度
+    float playerDirection = 1; // 自機の向き　　1で右向き
+    public GameObject attack1Prefab;
 
     void Start()
     {
@@ -15,39 +17,62 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
-        float move = 0;
-
-        // 右に移動
-        if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            move = 1;
-        }
-
-        // 左に移動
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            move = -1;
-        }
-
-
-        transform.Translate(this.speed * move * Time.deltaTime, 0, 0);
-
-       
-
+        move();
+        jump();
+        attack1();
+        attack2();
     }
 
-    void FixedUpdate()
+
+
+    void jump() // ジャンプ
     {
-        // ジャンプ
         if (Keyboard.current.spaceKey.wasPressedThisFrame && this.PlayerRigid.linearVelocityY == 0)
         {
             this.PlayerRigid.AddForce(transform.up * this.jumpforce);
         }
-
-        // 通常攻撃
-        if (Keyboard.current.cKey.wasPressedThisFrame)
+    }
+    void move() // 左右移動
+    {
+        float move = 0;
+        // 右に移動
+        if (Keyboard.current.rightArrowKey.isPressed)
         {
+            move = 1;
+            playerDirection = 1;
+        }
+        // 左に移動
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            move = -1;
+            playerDirection = -1;
+        }
+        transform.Translate(this.speed * move * Time.deltaTime, 0, 0);
+    }
 
+    void attack1()
+    {
+        // 通常攻撃
+        if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            if (playerDirection == 1)
+            {
+                Debug.Log("攻撃1右");
+                Instantiate(attack1Prefab,
+                transform.position + Vector3.right,
+                Quaternion.identity);
+            }
+            else Debug.Log("攻撃1左");
+        }
+    }
+
+    void attack2()
+    {
+        // 強攻撃
+        if (Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            if (playerDirection == 1) Debug.Log("攻撃2右");
+            else Debug.Log("攻撃2左");
         }
     }
 }
