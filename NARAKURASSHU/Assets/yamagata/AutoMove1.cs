2567cd022458f3f05931368d421049454f7ef755
yamagatamasaki <fko2647067@stu.o-hara.ac.jp>
2026-05-18 19:04:40 +0900

    up

--- NARAKURASSHU/Assets/yamagata/AutoMove1.cs
+++ NARAKURASSHU/Assets/yamagata/AutoMove1.cs
@@ -1,26 +1,44 @@
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class AutoMove : MonoBehaviour
- {
+{
     public float speed = 1.0f;
-    private int direction = 1; 
+    private int direction = 1;
 
     void Update()
     {
-        transform.position += new Vector3(direction*speed * Time.deltaTime, 0, 0);
+        transform.Translate(Vector2.right * direction * speed * Time.deltaTime);
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("当たる");
+
+        // wall タグに当たった時だけ反転
+
         if (collision.gameObject.CompareTag("wall"))
-        
-            direction = -1;
+        {
+            direction *= -1;
+        }
+
+
+
+        // キャラの向きを反転
 
         Vector3 scale = transform.localScale;
-        scale.x = -1;
+        scale.x *= -1;
         transform.localScale = scale;
-    
-  }
+
+
+
+    // プレイヤーの攻撃に当たったら消える
+    if(collision.gameObject.CompareTag("PlayerAttack"))
+       {
+          Destroy(gameObject);
+       }
+   }
 }
