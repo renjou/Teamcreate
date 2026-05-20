45f9eb84c88666c3b9aeea2840cc34342425ca87
yamagatamasaki <fko2647067@stu.o-hara.ac.jp>
2026-05-20 17:04:08 +0900

    up

--- NARAKURASSHU/Assets/yamagata/AutoMove1.cs
+++ NARAKURASSHU/Assets/yamagata/AutoMove1.cs
@@ -8,6 +8,9 @@ public class AutoMove : MonoBehaviour
     public float speed = 1.0f;
     private int direction = 1;
 
+    // HP
+    public int hp = 1;
+
     void Update()
     {
         transform.Translate(Vector2.right * direction * speed * Time.deltaTime);
@@ -16,29 +19,52 @@ public class AutoMove : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("当たる");
+        
 
-        // wall タグに当たった時だけ反転
+        // wall タグに当たった時だけ反転＆キャラの向きも反転
 
         if (collision.gameObject.CompareTag("wall"))
         {
             direction *= -1;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
         }
 
 
 
-        // キャラの向きを反転
 
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
+        // プレイヤーの攻撃に当たったら消える
+        if (collision.gameObject.CompareTag("PlayerAttack"))
+        {
+            hp--;
 
 
+            Debug.Log("Enemy HP : " + hp);
 
-    // プレイヤーの攻撃に当たったら消える
-    if(collision.gameObject.CompareTag("PlayerAttack"))
-       {
-          Destroy(gameObject);
-       }
-   }
+            // HPが0以下なら消える
+            if (hp <= 0)
+
+            {
+                Destroy(gameObject);
+
+                //プレイヤーに当たった
+
+                if (collision.gameObject.CompareTag("Player"))
+                {
+                    // PlayerHP取得
+
+                    PlayerHP playerHp = collision.gameObject.GetComponent<PlayerHP>();
+
+                    // ダメージ1
+
+                    if (playerHp != null)
+                    {
+                        playerHp.TakeDamage(1);
+
+                    }
+                }
+            }
+        }
+    }
 }
