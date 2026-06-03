7ca570a82fe8be83ba642f703ada35160fd45f7e
FunasakoYuga <fko2647071@stu.o-hara.ac.jp>
2026-06-03 16:52:08 +0900

    up

---
+++ NARAKURASSHU/Assets/funasako/SpecialAttack.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class SpecialAttack : MonoBehaviour
+{
+    Collider2D collider2d;
+    PlayerControl player;
+    enemy1 enemy;
+    Boss boss;
+
+    public float direction = 1;
+    float speed = 50;
+    void Start()
+    {
+        Destroy(gameObject, 1f);
+        collider2d = GetComponent<Collider2D>();
+        player = FindFirstObjectByType<PlayerControl>();
+        boss = FindFirstObjectByType<Boss>();
+        enemy = FindFirstObjectByType<enemy1>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(player);
+        Debug.Log(boss);
+        if (collision.CompareTag("enemy"))
+        {
+            Debug.Log("ヒット");
+            player.SpeGaugeIncrease();
+            Destroy(gameObject);
+            enemy.EnemyDamage(20);
+        }
+
+        if (collision.CompareTag("boss"))
+        {
+            Debug.Log("ボスヒット");
+            player.SpeGaugeIncrease();
+            Destroy(gameObject);
+            boss.BossDamage(20);
+        }
+    }
+
+    void Update()
+    {
+        if (direction == 1)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+}
