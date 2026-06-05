99cd32a559c2bc65e223ea2fbf8aaf8192d7a06e
寺山陽貴 <gaster2815@gmail.com>
2026-06-05 15:21:33 +0900

    taitle

--- NARAKURASSHU/Assets/terayama/TitleManager.cs
+++ NARAKURASSHU/Assets/terayama/TitleManager.cs
@@ -3,8 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    public GameObject controlsText;
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void ShowControls()
+    {
+        controlsText.SetActive(
+            !controlsText.activeSelf);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
