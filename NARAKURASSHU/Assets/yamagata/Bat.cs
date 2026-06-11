e74cc87172e511e989e332e6afe64978f4db47e3
yamagatamasaki <fko2647067@stu.o-hara.ac.jp>
2026-06-11 18:42:34 +0900

    anime

---
+++ NARAKURASSHU/Assets/yamagata/Bat.cs
@@ -0,0 +1,8 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Bot", menuName = "Scriptable Objects/Bot")]
+public class Bot : ScriptableObject
+{
+     public float 
+}
