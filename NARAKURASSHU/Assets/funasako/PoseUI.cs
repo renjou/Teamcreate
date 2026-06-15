a1f8b30f5d3cbdc1c9a828f0741195cb1852dbd4
FunasakoYuga <fko2647071@stu.o-hara.ac.jp>
2026-06-15 18:49:22 +0900

    ポーズ実装1

---
+++ NARAKURASSHU/Assets/funasako/PoseUI.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Accessibility;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
+public class PoseUI : MonoBehaviour
+{
+    public PlayerControl playerControl;
+
+    void Update()
+    {
+        PoseBoot();
+        PoseEnd();
+    }
+    void PoseBoot()
+    {
+        if (Keyboard.current.tKey.wasPressedThisFrame && playerControl.pose == false)
+        {
+            playerControl.pose = true;
+            Debug.Log("ポーズ中");
+        }
+    }
+
+    void PoseEnd()
+    {
+        if (Keyboard.current.tKey.wasPressedThisFrame && playerControl.pose == true)
+        {
+            playerControl.pose = false;
+            Debug.Log("ポーズ終了");
+        }
+    }
+}
