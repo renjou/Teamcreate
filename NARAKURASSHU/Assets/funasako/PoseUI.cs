1ab6f7cb80571429aa99311d7b5b73fb4a52a823
FunasakoYuga <fko2647071@stu.o-hara.ac.jp>
2026-06-17 16:06:09 +0900

    ポーズ画面追加

--- NARAKURASSHU/Assets/funasako/PoseUI.cs
+++ NARAKURASSHU/Assets/funasako/PoseUI.cs
@@ -2,34 +2,113 @@ using UnityEngine;
 using UnityEngine.Accessibility;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PoseUI : MonoBehaviour
 {
     public PlayerControl playerControl;
-    bool poseCoolTime = false;
+    public GameObject poseScreen;
+    public TextMeshProUGUI resume;
+    public TextMeshProUGUI title;
+    public RectTransform cursor;
+    public AudioClip poseSE;
+    public AudioClip cursorSE;
+    AudioSource audioSorce;
 
-    void Update()
+    private int selected = 0;
+
+    private void Start()
+    {
+        poseScreen.SetActive(false);
+        audioSorce = GetComponent<AudioSource>();
+    }
+    private void Update()
     {
         PoseBoot();
-        PoseEnd();
+        PoseCont();
     }
+
     void PoseBoot()
     {
-        if (Keyboard.current.tKey.wasPressedThisFrame && playerControl.pose == false)
+        if (Keyboard.current.tKey.wasPressedThisFrame && playerControl.ispose == false)
         {
-            playerControl.pose = true;
-            Debug.Log("ポーズ中");
+            Debug.Log("ポーズ起動");
+            playerControl.ispose = true;
+            Time.timeScale = 0;
+            poseScreen.SetActive(true);
+            audioSorce.PlayOneShot(poseSE);
         }
-    }
-
-    void PoseEnd()
-    {
-        if (Keyboard.current.tKey.wasPressedThisFrame && playerControl.pose == true)
+        /*
+        else if (Keyboard.current.tKey.wasPressedThisFrame && playerControl.ispose == true)
         {
-            playerControl.pose = false;
             Debug.Log("ポーズ終了");
+            playerControl.ispose = false;
+            Time.timeScale = 1;
+            poseScreen.SetActive(false);
+        }
+        */
+    }
+
+    void PoseCont()
+    {
+        if (Keyboard.current.downArrowKey.wasPressedThisFrame)
+        {
+            selected++;
+
+            audioSorce.PlayOneShot(cursorSE);
+            if (selected > 1)
+                selected = 0;
+
+            UpdateMenu();
+        }
+
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+        {
+            selected--;
+
+            audioSorce.PlayOneShot(cursorSE);
+            if (selected < 0)
+                selected = 1;
+
+            UpdateMenu();
+        }
+
+        if (Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            SelectMenu();
         }
     }
 
+    void UpdateMenu()
+    {
+        switch (selected)
+        {
+            case 0:
+                cursor.position =
+                    resume.rectTransform.position + new Vector3(-193f, 26f, 0f);
+                break;
 
+            case 1:
+                cursor.position =
+                    title.rectTransform.position + new Vector3(-236.9f, 26f, 0f);
+                break;
+        }
+    }
+
+    void SelectMenu()
+    {
+        switch (selected)
+        {
+            case 0:
+                playerControl.ispose = false;
+                Time.timeScale = 1;
+                poseScreen.SetActive(false);
+                break;
+
+            case 1:
+                SceneManager.LoadScene("Title");
+                break;
+        }
+    }
 }
