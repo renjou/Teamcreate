8a903c1e7a2e35deca2e5cd11bb2ea14211160cb
renjou <fko2647042@stu.o-hara.ac.jp>
2026-07-14 17:24:33 +0900

    st

---
+++ NARAKURASSHU/Assets/renjou/CameraShake.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+
+  public static CameraShake Instance { get; private set; }
+    private Vector3  originalPosition;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+   private void OnEnable()
+    {
+        originalPosition = transform.localPosition;
+    }
+    private void Shake(float duration, float magnitude)
+    {
+        StopAllCoroutines();
+        StartCoroutine(DoShake(duration, magnitude));
+    }
+    private IEnumerator DoShake(float duration, float magnitude)
+    {
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            // ランダムな位置を計算（2DなのでZ軸は固定）
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
+
+            elapsed += Time.deltaTime;
+
+            // 1フレーム待つ
+            yield return null;
+        }
+
+       
+        transform.localPosition = originalPosition;
+        // Update is called once per frame
+    }
+}
